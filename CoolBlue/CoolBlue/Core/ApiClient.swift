2f8d7446ea6468b5cf46aff75f61e8e329c7c743
dmandarino <Douglas Mandarino>
2018-05-27 17:19:26 -0300

    create ApiClient

--- CoolBlue/CoolBlue/Core/ApiClient.swift
+++ CoolBlue/CoolBlue/Core/ApiClient.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2018 Douglas Mandarino. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+
+protocol ApiClientProtocol {
+    func fetch(endpoint: String, completion: @escaping ([String: Any]?) -> ())
+}
+
+class ApiClient: ApiClientProtocol {
+    
+    static let sharedInstance = ApiClient()
+    
+    private var requestURL: String = ""
+    private let hostiOSAssignmentURL = "https://bdk0sta2n0.execute-api.eu-west-1.amazonaws.com/ios-assignment"
+    
+    private init() {}
+    
+    func fetch(endpoint: String, completion: @escaping ([String: Any]?) -> ()) {
+        setRequestURL(forEndpoint: endpoint)
+        Alamofire.request(requestURL).validate().responseJSON { response in
+            guard response.result.isSuccess else {
+                print("Error while fetching in \(self.requestURL). Error Message: \(String(describing: response.result.error))")
+                return
+            }            
+            guard let value = response.result.value as? [String: Any] else {
+                return
+            }
+            completion(value)
+        }
+    }
+    
+    private func setRequestURL(forEndpoint endpoint: String) {
+        requestURL = hostiOSAssignmentURL + endpoint
+    }
+}
