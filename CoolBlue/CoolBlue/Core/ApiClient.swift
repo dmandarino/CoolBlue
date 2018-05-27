deecfb3e65d0e2563c97dbfc1d658d82cb1437d5
dmandarino <Douglas Mandarino>
2018-05-27 17:16:31 -0300

    add Alamofire

---
+++ CoolBlue/CoolBlue/Core/ApiClient.swift
@@ -0,0 +1,9 @@
+//
+//  ApiClient.swift
+//  CoolBlue
+//
+//  Created by Douglas Mandarino on 27/05/18.
+//  Copyright © 2018 Douglas Mandarino. All rights reserved.
+//
+
+import Foundation
---
+++ CoolBlue/CoolBlueTests/Core/XCTestCaseExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  XCTestCaseExtensions.swift
+//  CoolBlueTests
+//
+//  Created by Douglas Mandarino on 27/05/18.
+//  Copyright © 2018 Douglas Mandarino. All rights reserved.
+//
+
+import Foundation
