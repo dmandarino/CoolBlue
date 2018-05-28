9881112adefa194133a1b8ed9bb0551cfdc22613
dmandarino <Douglas Mandarino>
2018-05-28 01:11:42 -0300

    Add Entities to project

---
+++ CoolBlue/CoolBlue/Core/Product/Product.swift
@@ -0,0 +1,23 @@
+//
+//  Product.swift
+//  CoolBlue
+//
+//  Created by Douglas Mandarino on 28/05/18.
+//  Copyright © 2018 Douglas Mandarino. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    var productId: Int
+    var productName: String
+    var reviewInformation: Review
+    var usp: [String]
+    var salesPriceIncVat: Int
+    var productImage: String
+}
+
+struct Review {
+    var reviewAverage: Float?
+    var reviewCount: Int?
+}
