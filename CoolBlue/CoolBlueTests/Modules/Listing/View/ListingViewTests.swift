de2d27297d71c89e28808dec2d30643fdd1c186b
dmandarino <Douglas Mandarino>
2018-05-28 23:34:22 -0300

    add tests to view

---
+++ CoolBlue/CoolBlueTests/Modules/Listing/View/ListingViewTests.swift
@@ -0,0 +1,9 @@
+//
+//  ListingViewTests.swift
+//  CoolBlueTests
+//
+//  Created by Douglas Mandarino on 28/05/18.
+//  Copyright © 2018 Douglas Mandarino. All rights reserved.
+//
+
+import Foundation
