1aed42ee2e654595240cd77312ec0421255bb9c6
Douglas Pinheiro Mandarino <douglas.mandarino@olxbr.com>
2018-05-29 23:14:23 -0300

    create xib for listingView and set it as primary screen

--- CoolBlue/CoolBlue/AppDelegate.swift
+++ CoolBlue/CoolBlue/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: ListingView())
+        navigationController.navigationBar.barTintColor = UIColor.AppColor.blue
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
--- CoolBlue/CoolBlue/Modules/Listing/View/ListingView.swift
+++ CoolBlue/CoolBlue/Modules/Listing/View/ListingView.swift
@@ -12,12 +12,25 @@ import UIKit
 //MARk: - ListingView
 
 class ListingView: UIViewController {
-    
+
     var presenter: ListingPresenterProtocol?
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    var labels = ["label1", "label2", "label3"]
+    
+    convenience init() {
+        self.init(nibName: "ListingView", bundle: nil)
+    }
     
     override func viewDidLoad() {
         ListingConfigurator.configure(viewController: self)
+        setupScreen()
+    }
+    
+    private func setupScreen() {
         updateView()
+        setupCollectionView()
     }
 }
 
@@ -51,5 +64,30 @@ extension ListingView: ListingPresenterOutputProtocol {
         alertController.addAction(tryAgainAction)
         alertController.addAction(dissmissAction)
         self.present(alertController, animated: true, completion: nil)
+    }
+}
+
+extension ListingView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.black
+        return cell
+    }
+    
+    private func setupCollectionView() {
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 }
