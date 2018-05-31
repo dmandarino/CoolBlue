25b74b2b5f5f53b30482b2a5671f978961a542d9
dmandarino <Douglas Mandarino>
2018-05-31 15:48:16 -0300

    Use SkeletonView as placholder in Cells

--- CoolBlue/CoolBlue/Modules/Listing/View/ListingView.swift
+++ CoolBlue/CoolBlue/Modules/Listing/View/ListingView.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SkeletonView
 
 //MARk: - ListingView
 
@@ -16,6 +17,7 @@ class ListingView: UIViewController {
     var presenter: ListingPresenterProtocol?
     private var productList: [Product] = []
     private var filteredProducList: [Product] = []
+    private let reuseIdentifier = "cell"
     
     private let searchController = UISearchController(searchResultsController: nil)
     
@@ -40,21 +42,30 @@ class ListingView: UIViewController {
 //MARk: - UICollectionView
 
 extension ListingView: UICollectionViewDelegate, UICollectionViewDataSource {
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if productList.isEmpty {
+            return 10
+        }
         if isFiltering() {
             return filteredProducList.count
         }
         return productList.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ProductCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ProductCell
+        let labelsToBeSkeltonable: [UILabel] = [cell.productName, cell.productPrice]
         
         guard isProductListNotEmpty() else {
+            setSkeletonableLabels(labels: labelsToBeSkeltonable)
             return cell
         }
         
+        hideSkeletonsForLabels(labels: labelsToBeSkeltonable)
+        return cellConfigured(cell: cell, forIndex: indexPath.row)
+    }
+    
+    private func cellConfigured(cell: ProductCell, forIndex index: Int) -> ProductCell {
         var collection: [Product] = []
         
         if isFiltering() {
@@ -63,18 +74,17 @@ extension ListingView: UICollectionViewDelegate, UICollectionViewDataSource {
             collection = productList
         }
         
-        let url = URL(string: collection[indexPath.row].productImage)!
-        cell.productName.text = collection[indexPath.row].productName
-        cell.productPrice.text = collection[indexPath.row].salesPriceIncVat.currency
+        cell.productName.text = collection[index].productName
+        cell.productPrice.text = collection[index].salesPriceIncVat.currency
         cell.productImage.af_setImage(
-            withURL: url,
+            withURL: URL(string: collection[index].productImage)!,
             placeholderImage: UIImage(named: "placeholder"),
             imageTransition: .crossDissolve(0.5)
         )
         
         return cell
     }
-    
+
     private func isProductListNotEmpty() -> Bool {
         return !productList.isEmpty
     }
@@ -84,7 +94,7 @@ extension ListingView: UICollectionViewDelegate, UICollectionViewDataSource {
     }
     
     private func setupCollectionView() {
-        self.collectionView.register(UINib.init(nibName: "ProductCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+        self.collectionView.register(UINib.init(nibName: "ProductCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
     }
@@ -121,6 +131,28 @@ extension ListingView: ListingPresenterOutputProtocol {
         alertController.addAction(tryAgainAction)
         alertController.addAction(dissmissAction)
         self.present(alertController, animated: true, completion: nil)
+    }
+}
+
+//MARK: - SkeletonTableViewDataSource
+
+extension ListingView: SkeletonCollectionViewDataSource {
+    
+    func collectionSkeletonView(_ skeletonView: UICollectionView, cellIdentifierForItemAt indexPath: IndexPath) -> ReusableCellIdentifier {
+        return reuseIdentifier
+    }
+    
+    private func setSkeletonableLabels(labels: [UILabel]) {
+        for label in labels {
+            label.isSkeletonable = true
+            label.showAnimatedGradientSkeleton()
+        }
+    }
+    
+    private func hideSkeletonsForLabels(labels: [UILabel]) {
+        for label in labels {
+            label.hideSkeleton()
+        }
     }
 }
 
